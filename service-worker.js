9919b12408a3a831fdc01ad7de97bff403f06f5c
tsferran-cmyk <ts.ferran@gmail.com>
2026-06-21 21:03:38 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='japan-activities-v8';
+const ASSETS=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
